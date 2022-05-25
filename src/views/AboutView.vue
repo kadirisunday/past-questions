b1ce000f75bb16a2f4139efe8111419009da7960
kadiri sunday <kadirisunday212@gmail.com>
2022-05-25 16:43:38 +0100

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div class="d-grid gap-2">
+  <button class="btn btn-primary" type="button">Button</button>
+  <button class="btn btn-primary" type="button">Button</button>
+</div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    
+  </main>
+</template>
+
+
+<script setup>
+
+</script>
+
+
